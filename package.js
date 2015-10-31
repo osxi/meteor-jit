05bbdb613488c58d886a922614cbf41a851e0db0
osxi <osxi@users.noreply.github.com>
2015-10-30 20:45:12 -0500

    add jit

---
+++ package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'osxi:jit',
+  version: '2.0.1',
+  summary: 'JIT (JavaScript InfoVis Toolkit) packaged for Meteor',
+  git: 'git@github.com:osxi/meteor-jit.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('jit-yc.js', 'client');
+  api.addFiles('jit.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('osxi:jit');
+  api.addFiles('jit-tests.js');
+});
